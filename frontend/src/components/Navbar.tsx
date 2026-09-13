b0c361567eaa671a0df1c3d22194e374cb59fe5a
SaphalKhatri <Khatrisaphal11@gmail.com>
2026-09-13 11:31:52 +0545

    s

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         {/* Logo */}
         <Link to="/" className="text-2xl font-bold text-gray-800">
-          MyPortfolio
+          <h1>Saphal Kumar Khatri</h1>
         </Link>
 
         {/* Navigation Links */}
